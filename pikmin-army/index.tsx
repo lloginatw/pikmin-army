53695ab8b3e3b5b65d405b623ef95a45fd8d9a8e
lloginatw <lloginatw@gmail.com>
2026-01-20 22:28:13 +0800

    Update index.tsx

--- pikmin-army/index.tsx
+++ pikmin-army/index.tsx
@@ -2,14 +2,10 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
-const container = document.getElementById("root");
-const root = createRoot(container!);
+const container = document.getElementById("root")!;
+const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+<React.StrictMode> <App />
+</React.StrictMode>
 );
-export default function App() {
-  return <div className="p-4 text-center text-xl">Hello World!</div>;
-}
